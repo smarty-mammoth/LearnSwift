be84a3e45e05517b11da6c482010d1c16b8fe747
smarty-mammoth <smarty-mammoth@mail.ru>
2020-03-01 20:51:08 +0400

    Implementation of Rank enum

--- LearnSwift.playground/Contents.swift
+++ LearnSwift.playground/Contents.swift
@@ -5,5 +5,24 @@ enum Rank : Int {
     case ace = 1
     case two, three, four, five, six, seven, eight, nine, ten
     case jack, queen, king
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "Ace"
+        case .jack:
+            return "Jack"
+        case .queen:
+            return "Queen"
+        case .king:
+            return "King"
+        default:
+            return String(self.rawValue)
+        }
+    }
 }
+
+let rank = Rank.ace
+let iRank = rank.rawValue
+let descr = rank.simpleDescription()
 
